512b808e4383fdc36bfbc6dc8cde27917dd58c56
yichunzhao <zhaoyichun@hotmail.com>
2020-12-04 00:43:08 +0100

    jdbc driver connecting to the db

---
+++ src/main/java/com/ynz/jdbc/DatabaseConnectionManager.java
@@ -0,0 +1,23 @@
+package com.ynz.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnectionManager {
+    private final String url;
+    private final Properties properties;
+
+    public DatabaseConnectionManager(String hostname, String databaseName, String username, String password) {
+        this.url = "jdbc:postgresql://" + hostname + "/" + databaseName;
+        this.properties = new Properties();
+        this.properties.setProperty("user", username);
+        this.properties.setProperty("password", password);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, properties);
+    }
+
+}
--- src/main/java/com/ynz/jdbc/JDBCExecutor.java
+++ src/main/java/com/ynz/jdbc/JDBCExecutor.java
@@ -1,8 +1,31 @@
 package com.ynz.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
 
     public static void main(String[] args) {
         System.out.println("using jdbc...");
+        String host = "localhost";
+        String databaseName = "favoritecolors";
+        String userName = "postgres";
+        String password = "test";
+        DatabaseConnectionManager dbManager = new DatabaseConnectionManager(host, databaseName, userName, password);
+
+        try {
+            Connection con = dbManager.getConnection();
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery("select count(*) from colors");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
